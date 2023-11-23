5b5af5bdd98f17180576a71510dfd60667517582
Danna Paola <dannacapera16@gmail.com>
2023-11-23 10:34:10 -0500

    Relationship of models

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Curso extends Model
 {
     use HasFactory;
+
+    public $table = "cursos";
+    protected $fillable = array("*"); //all the fields in the table
+
+    //method that returns the list of students that a course has
+    public function estudiantes(){
+        return $this->belongsToMany(Estudiante::class,"curso_estudiante");
+    }
+    public $timestamps = true;
 }
--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Estudiante extends Model
 {
     use HasFactory;
+
+    public $table = "estudiantes";
+    protected $fillable = array("*"); //all the fields in the table
+
+    //method that returns the list of courses that a student has
+    public function estudiantes(){
+        return $this->belongsToMany(Curso::class,"curso_estudiante");
+    }
 }
