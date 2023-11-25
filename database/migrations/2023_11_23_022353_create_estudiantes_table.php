41f7316d741ec6bd17b0daa3943ecb4f63feb132
Danna Paola <dannacapera16@gmail.com>
2023-11-24 22:24:23 -0500

    LongText to save photos

--- database/migrations/2023_11_23_022353_create_estudiantes_table.php
+++ database/migrations/2023_11_23_022353_create_estudiantes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("nombre");
             $table->string("apellido");
-            $table->text("foto");
+            $table->longText("foto");
             $table->timestamps();
         });
     }
